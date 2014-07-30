46002c246a5f951a84149f727e8ce2a18a49241d
Alex Zvoleff <azvoleff@gmail.com>
2014-07-30 16:10:27 -0400

    Fis NA issue noted by Ramesh.

--- convert_training_to_txt.R
+++ convert_training_to_txt.R
@@ -3,14 +3,16 @@
 library(foreach)
 library(iterators)
 
+library(R.utils)
 library(stringr)
 library(tools)
 library(rgdal)
 library(rgeos)
 library(dplyr)
+library(reshape2)
 
 input_dir <- 'H:/Data/Landsat/LCLUC_Training'
-out_dir <- 'H:/Data/Landsat/Training_targz_files'
+out_dir <- 'H:/Data/Landsat/Vertica_Hackathon/Training_targz_files'
 #input_dir <- 'H:/Data/Landsat/LCLUC_Classifications'
 
 training_files <- dir(input_dir, pattern='_Landsat_Training_Data.shp', 
@@ -28,26 +30,30 @@ retvals <- foreach (training_file=iter(training_files),
                      file_path_sans_ext(basename(training_file)))
 
     sitecode <- str_extract(basename(training_file), '[a-zA-Z]*')
-    image_proj4string <- img_metadata[match(sitecode, img_metadata$sitecode), 
-                                      ]$proj4string
+    image_proj4string <- img_metadata[match(sitecode, img_metadata$sitecode), ]$proj4string
     polys <- spTransform(polys, CRS(image_proj4string))
 
     polydata <- polys@data
     polydata <- polydata[, order(names(polydata))]
     polydata <- cbind(poly_coords=writeWKT(polys, byid=TRUE), polydata)
+
+    class_cols <- grep('Class', names(polydata))
+    polydata[class_cols] <- apply(polydata[class_cols], 2, as.character)
+
     polydata <- melt(polydata, id.vars=c("poly_coords", "UUID"), 
-                     measure.vars=grep('Class',names(polydata)), 
-                     variable.name="year", value.name="class")
+                     measure.vars=class_cols, variable.name="datayear", 
+                     value.name="class")
 
     # There should be a polygon for each year at this point
     stopifnot(length(polydata) != (5 * length(polys)))
 
-    polydata$year <- as.numeric(gsub('Class_', '', polydata$year))
+    polydata$datayear <- as.numeric(gsub('Class_', '', polydata$datayear))
     polydata <- polydata[!polydata$class == "Unknown", ]
     polydata <- cbind(polyid=seq(0, nrow(polydata) - 1), polydata)
 
     txt_filename <- file.path(out_dir, paste0(file_path_sans_ext(basename(training_file)), ".txt"))
     write.table(polydata, file=txt_filename, row.names=FALSE, quote=FALSE, sep="|")
     gzip(txt_filename, remove=TRUE, overwrite=TRUE)
+
 }
 timestamp()
