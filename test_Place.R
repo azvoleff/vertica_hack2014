2b243ccf84dcc194a201f3a25e565c2a04612f80
Alex Zvoleff <azvoleff@conservation.org>
2014-07-31 09:21:08 -0400

    Update Place testing code.

--- test_Place.R
+++ test_Place.R
@@ -16,3 +16,10 @@ pred_cols <- c("r1", "r2", "r3", "r4", "r5", "r7", "veg", "vegmean", "vegvar",
 
 library(rgdal)
 library(rgeos)
+
+con <- odbcConnect("hack14")
+
+pred_cols_paste <- paste(pred_cols, collapse=", ")
+a <- sqlQuery(con, "SELECT STV_Create_Index(polyid, poly_coords USING PARAMETERS index='tr_polys') OVER() FROM CI.PSH_landsat")
+b <- sqlQuery(con, "SELECT STV_Intersect(rowid, pixel_coord USING PARAMETERS index='tr_polys') OVER() AS (rowid, polyid) FROM CI.PSH_predictor")
+c <- sqlQuery(con, paste("SELECT", pred_cols_paste, "STV_Intersect(rowid, pixel_coord USING PARAMETERS index='tr_polys') OVER() AS (rowid, polyid) FROM CI.PSH_predictor")
